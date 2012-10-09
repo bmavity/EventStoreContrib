3c39dfe8b5f09d528f688de97e4154e171495531
bmavity <brian@brianmavity.com>
2012-10-08 22:50:24 -0500

    Initial source code commit

---
+++ EventStore.SingleNode.Topshelf/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Topshelf;
+
+namespace EventStore.SingleNode.Topshelf
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<SingleNodeWithProjections>(s =>
+                {
+                    s.ConstructUsing(name => new SingleNodeWithProjections());
+                    s.WhenStarted(tc => tc.Run(new [] { "--db", "c:\\temp\\db", "-t", "3000", "--ip", "127.0.0.1", "-h", "3001", }));
+                    s.WhenStopped(tc => tc.Stop());
+                });
+
+                x.UseNLog();
+
+                x.RunAsNetworkService();
+                x.SetDescription("EventStore Topshelf");
+                x.SetDisplayName("EventStore");
+                x.SetServiceName("EventStore");
+            });
+        }
+    }
+}
